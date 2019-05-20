f010b5036e2ca062c13c2267bbc22f24d2d77052
18swenskiq <18swenskiq@gmail.com>
2019-05-19 20:44:47 -0400

    Added command handler module, cleaned up main

---
+++ SquidskiBot/SquidskiBotMain/CommandHandler.py
@@ -0,0 +1,47 @@
+import discord
+import sys
+
+# Makes the commands section work
+sys.path.append('./Commands')
+
+# Commands
+from help import help
+from pings import pings
+from purge import purge
+from seinfeldme import seinfeldme
+from sws import sws
+
+class CommandHandler():
+    async def commandParser(self, message, globalCall):
+
+        # Help message
+        if (message.content.lower() == globalCall + "help"):
+            myHelp = help()
+            await message.channel.send(embed = myHelp.myEmbed())
+            print("Sent help message for " + str(message.author))
+
+        # Search workshop
+        if (message.content.startswith((globalCall + 'sws'))):
+            searchWorkshop = sws()
+            await message.channel.send("Searching the workshop. Please wait...")
+            searchIt = searchWorkshop.theMain(message.content)
+            if not searchIt:
+                await message.channel.send("That's a fat error from me dawg. The search came up empty.")
+            else:
+                await message.channel.send(searchIt)
+
+        # Get random Seinfeld quote
+        if (message.content.startswith((globalCall + 'seinfeldme'))):
+            getSeinfeldQuote = seinfeldme()
+            await message.channel.send(getSeinfeldQuote.getQuote())
+            print("Sent a Seinfeld quote for " + str(message.author))
+
+        # Give/Remove Pings role
+        if (message.content.lower().startswith((globalCall + 'pings'))):
+            myPings = pings()
+            await myPings.changePingRoleState(message)
+
+        # Purge messages (Administrator Only)
+        if (((message.content.lower()).split(" "))[0].startswith(globalCall + "purge")):
+            myPurge = purge()
+            await myPurge.purger(message)
--- SquidskiBot/SquidskiBotMain/SquidskiBotMain.py
+++ SquidskiBot/SquidskiBotMain/SquidskiBotMain.py
@@ -5,27 +5,13 @@ import logging
 
 # Various other files
 from ChiefMuteInsurance import ChiefMuteInsurance
+from CommandHandler import CommandHandler
 
-# Makes the commands section work
-sys.path.append('./Commands')
-
-# Commands
-from help import help
-from pings import pings
-from purge import purge
-from seinfeldme import seinfeldme
-from sws import sws
-
-# This string can be modified to change what prefix the bot responds to
+# Picks the symbol to prefix onto commands
 globalCall = ">"
 
 # Initialization alerts
 print("The call symbol for the bot is " + globalCall)
-print("Successfully imported help command module")
-print("Successfully imported pings command module")
-print("Successfully imported purge command module")
-print("Successfully imported seinfeldme command module")
-print("Successfully imported sws command module")
 
 # Logger setup
 logger = logging.getLogger('discord')
@@ -51,46 +37,19 @@ class MyClient(discord.Client):
         if message.author == self.user:
             return
 
-        # Help message
-        if (message.content.lower() == globalCall + "help"):
-            myHelp = help()
-            await message.channel.send(embed = myHelp.myEmbed())
-            print("Sent help message for " + str(message.author))
+        # Handle the Commands
+        if (message.content.startswith(globalCall)):
+            myCommand = CommandHandler()
+            await myCommand.commandParser(message, globalCall)
 
         # Good night response
         if (message.content.lower() in ["good night","gn","goodnight"]):
             await (message.channel.send("Good night " + str(message.author)[:-5]))
 
-        # Search workshop
-        if (message.content.startswith((globalCall + 'sws'))):
-            searchWorkshop = sws()
-            await message.channel.send("Searching the workshop. Please wait...")
-            searchIt = searchWorkshop.theMain(message.content)
-            if not searchIt:
-                await message.channel.send("That's a fat error from me dawg. The search came up empty.")
-            else:
-                await message.channel.send(searchIt)
-
-        # Get random Seinfeld quote
-        if (message.content.startswith((globalCall + 'seinfeldme'))):
-            getSeinfeldQuote = seinfeldme()
-            await message.channel.send(getSeinfeldQuote.getQuote())
-            print("Sent a Seinfeld quote for " + str(message.author))
-
         # Mute if ping chief
         if ("<@208272642640314389>" in message.content):
             chiefMute = ChiefMuteInsurance()
             await chiefMute.the_muter(message)
-
-        # Give/Remove Pings role
-        if (message.content.lower().startswith((globalCall + 'pings'))):
-            myPings = pings()
-            await myPings.changePingRoleState(message)
-
-        # Purge messages (Administrator Only)
-        if (((message.content.lower()).split(" "))[0].startswith(globalCall + "purge")):
-            myPurge = purge()
-            await myPurge.purger(message)
 
 
 # Instantiate client
