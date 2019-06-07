59302fcda928701fab650f5b75e03b2f98afd3dc
18swenskiq <18swenskiq@gmail.com>
2019-06-06 23:14:14 -0400

    Additional documentation added for rolepinger.py

--- SquidskiBot/SquidskiBotMain/Commands/rolepinger.py
+++ SquidskiBot/SquidskiBotMain/Commands/rolepinger.py
@@ -4,11 +4,17 @@ class rolepinger():
     adminRoleID = "574763874201501696"
     
     async def pinger(self, message):
+
+        # Checks for Admin
          if self.adminRoleID in str(message.author.roles):
+             # Makes the Pings role mentionable
              await (discord.utils.get(message.guild.roles, name='Pings')).edit(mentionable=True)
+             # Sends ping
              await message.channel.send("<@&579453547976982566>")
              await message.channel.send("*To unsubscribe from pings, type >pings in <#574764733849272347>*")
+             # Makes the Pings role unmentionable
              await (discord.utils.get(message.guild.roles, name='Pings')).edit(mentionable=False)
+             print("Pings role was successfully pinged from " + str(message.channel))
          else:
             await message.channel.send("You must have the `Administrator` role to do this...")
             print(str(message.author) + " tried to use the rolepinger command...")
