2f250d22bb498d64ebd2480e5115c3d5f79e75ec
18swenskiq <18swenskiq@gmail.com>
2019-07-05 20:02:21 -0400

    Pinging Ch(i)ef now only mutes for 1 minute

--- SquidskiBot/SquidskiBotMain/ChiefMuteInsurance.py
+++ SquidskiBot/SquidskiBotMain/ChiefMuteInsurance.py
@@ -3,15 +3,15 @@ import discord
 
 class ChiefMuteInsurance():
     
-    # If a user pings Ch(i)ef, they are muted (sent to void) for 5 minutes
+    # If a user pings Ch(i)ef, they are muted (sent to void) for 1 minute
     async def the_muter(self, msg):
         await msg.author.add_roles(discord.utils.get(msg.guild.roles, name='Muted'))
-        await msg.channel.send("For pinging Ch(i)ef, " + str(msg.author)[:-5] + " has been muted for 5 mintues.")
+        await msg.channel.send("For pinging Ch(i)ef, " + str(msg.author)[:-5] + " has been muted for 1 mintue.")
         print(str(msg.author) + " was muted because they pinged Ch(i)ef.")
-        await asyncio.sleep(300)
+        await asyncio.sleep(60)
         if("579453547976982566" in str(msg.author.roles)):
             await msg.author.remove_roles(discord.utils.get(msg.guild.roles, name='Muted'))
-            print(str(msg.author) + " was unmuted after 5 minutes for muting Ch(i)ef.")
+            print(str(msg.author) + " was unmuted after 1 minute for muting Ch(i)ef.")
             await msg.channel.send(str(msg.author)[:-5] + " has been unmuted after pinging Ch(i)ef. Please don't ping Ch(i)ef.")
         else:
             await msg.author.remove_roles(discord.utils.get(msg.guild.roles, name='Muted'))
