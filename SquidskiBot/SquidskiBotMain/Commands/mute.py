94a040e0b2f672f0aed42fd02a7837bc39d3a6d7
18swenskiq <18swenskiq@gmail.com>
2019-07-17 13:49:31 -0400

    Mute.py looks visually 100% cleaner

--- SquidskiBot/SquidskiBotMain/Commands/mute.py
+++ SquidskiBot/SquidskiBotMain/Commands/mute.py
@@ -5,7 +5,7 @@ from LoggingModule import LoggingModule
 
 class mute():
 
-    # TODO: Clean up this file by making the string f strings as well as not permamuting on >mute with nothing after it
+    # TODO: Fix the crashing of the task when you do >mute with none or not enough parameters
     async def mute_users(self,message):
         log = LoggingModule()
         with open('settings.json') as json_file:
@@ -16,14 +16,15 @@ class mute():
             return
         await message.mentions[0].add_roles(discord.utils.get(message.guild.roles, name='Muted'))
         if(message.content.split(" ")[2] == "1"):
-            await message.channel.send("Muted " + str(message.mentions[0]) + " for " + message.content.split(" ")[2] + " minute.")
+            # await message.channel.send(f"Muted " + str(message.mentions[0]) + " for " + message.content.split(" ")[2] + " minute.")
+            await message.channel.send(f'Muted {message.mentions[0]} for {message.content.split(" ")[2]} minute.')
         else:
-            await message.channel.send("Muted " + str(message.mentions[0]) + " for " + message.content.split(" ")[2] + " minutes.")
-        await log.logIt("Muted "  + str(message.mentions[0]) + " for " + str(message.content.split(" ")[2]) + " minutes.", message)
+            await message.channel.send(f'Muted {message.mentions[0]} for {message.content.split(" ")[2]} minutes.')
+        await log.logIt(f'Muted {message.mentions[0]} for {message.content.split(" ")[2]} minutes.', message)
         await asyncio.sleep(int(message.content.split(" ")[2]) * 60)
         if (settingsFile["mutedRole"] in str(message.mentions[0].roles)):
             await message.mentions[0].remove_roles(discord.utils.get(message.guild.roles, name='Muted'))
             await log.logIt(f"{message.mentions[0]} is now unmuted.", message)
-            await message.channel.send(str(message.mentions[0]) + " has been unmuted. Please be a good user.")
+            await message.channel.send(f"{message.mentions[0]} has been unmuted. Please be a good user.")
         else:
             await log.logIt(f"It seems that {message.mentions[0]} was already unmuted by an admin. No message will be sent by me.", message)
