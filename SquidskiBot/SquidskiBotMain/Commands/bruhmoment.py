946314ad2316e2884025f575d5d0722cdcdc291f
18swenskiq <18swenskiq@gmail.com>
2019-07-18 20:43:47 -0400

    Fixed bug and rolled out new error handling to bruhmoment

--- SquidskiBot/SquidskiBotMain/Commands/bruhmoment.py
+++ SquidskiBot/SquidskiBotMain/Commands/bruhmoment.py
@@ -1,18 +1,23 @@
 import discord
 from LoggingModule import LoggingModule
+from ErrorPrintingModule import ErrorPrintingModule
 import random
 
 class bruhmoment():
 
     # Rolls a random number to determine if something is a 'bruh' moment
     async def isBruhMoment(self, message):
-        log = LoggingModule()
-        myVal = random.random()
-        if (myVal > .5):
-            await self.classifyBruhMoment(message)
-        else:
-            await message.channel.send("I have determined that this is not a bruh moment.")
-            await log.logIt(f"Message wasn't a bruh moment", message)
+        try:
+            log = LoggingModule()
+            myVal = random.random()
+            if (myVal > .5):
+                await self.classifyBruhMoment(message)
+            else:
+                await message.channel.send("I have determined that this is not a bruh moment.")
+                await log.logIt(f"Message wasn't a bruh moment", message)
+        except:
+            e = ErrorPrintingModule()
+            await e.reportError(message, sys.exc_info()[0])
 
     # If it is a 'bruh' moment, reads and writes to file containing total amount of bruh moments
     async def classifyBruhMoment(self, message):
@@ -25,6 +30,6 @@ class bruhmoment():
         myFile.close()
         myFile = open("./BruhMoments.txt","w")
         myFile.write(str(int(bruhMoments) + 1))
-        await log.logIt("This server has had " + str(int(bruhMoments) + 1) + " bruh moments.")
+        await log.logIt("This server has had " + str(int(bruhMoments) + 1) + " bruh moments.", message)
         myFile.close()
 
