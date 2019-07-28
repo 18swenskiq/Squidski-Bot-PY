c0216769bc9686d7af12dbf6f3816fa2a893aef7
18swenskiq <18swenskiq@gmail.com>
2019-07-28 19:59:46 -0400

    Voice check module is fully deployed

--- SquidskiBot/SquidskiBotMain/CheckIfInVoice.py
+++ SquidskiBot/SquidskiBotMain/CheckIfInVoice.py
@@ -1,27 +1,32 @@
 import asyncio
 import discord
 import json
+import sys
 
 class CheckIfInVoice():
 
     async def periodic(self, myClient):
-        with open('settings.json') as json_file:
-            settingsFile = json.load(json_file)
-        # voiceChannel = settingsFile["voiceChannel"]
-        myGuild = myClient.guilds[1]
-        print(myGuild)
-        voiceChannel = discord.utils.get(myGuild.voice_channels, name="General", bitrate=64000)
-        voiceRole = discord.utils.get(myGuild.roles, name="In Voice")
-        whoHasTheVoiceRole = []
-        while True:
-            memberList = voiceChannel.members
-            for personConnected in memberList:
-                if settingsFile["voiceRole"] not in personConnected.roles:
-                    await personConnected.add_roles(voiceRole)
-                    whoHasTheVoiceRole.append(personConnected)
-            for memberInVoiceList in whoHasTheVoiceRole:
-                if memberInVoiceList not in memberList:
-                    await memberInVoiceList.remove_roles(voiceRole)
-            await asyncio.sleep(5)
+        try:
+            sys.setrecursionlimit(999999999)
+            with open('settings.json') as json_file:
+                settingsFile = json.load(json_file)
+            myGuild = myClient.guilds[1]
+            voiceChannel = discord.utils.get(myGuild.voice_channels, name="General", bitrate=64000)
+            voiceRole = discord.utils.get(myGuild.roles, name="In Voice")
+            whoHasTheVoiceRole = []
+            iterations = 0
+            while iterations < 5:
+                memberList = voiceChannel.members
+                for personConnected in memberList:
+                    if settingsFile["voiceRole"] not in personConnected.roles:
+                        await personConnected.add_roles(voiceRole)
+                        whoHasTheVoiceRole.append(personConnected)
+                for memberInVoiceList in whoHasTheVoiceRole:
+                    if memberInVoiceList not in memberList:
+                        await memberInVoiceList.remove_roles(voiceRole)
+                await asyncio.sleep(1)
+                iterations += 1
+            await self.periodic(myClient)
 
-
+        except:
+            await self.periodic(myClient)
